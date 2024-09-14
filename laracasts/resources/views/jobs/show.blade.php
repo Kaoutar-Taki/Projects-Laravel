5cdc08d21545581e90a605037118e38bd425234a
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-09-14 10:17:35 +0100

    authorization

--- laracasts/resources/views/jobs/show.blade.php
+++ laracasts/resources/views/jobs/show.blade.php
@@ -4,8 +4,10 @@
     </x-slot:heading>
     <h2 class="font-bold text-lg">{{ $job->title }}</h2>
     <p>This job pays {{ $job->salary }} per year.</p>
-    <p class="mt-6">
-        <x-button href="/jobs/{{ $job->id }}/edit" class='bg-blue-500 hover:bg-blue-600 text-white'>Edit
-            Job</x-button>
-    </p>
+    @can('edit', $job)
+        <p class="mt-6">
+            <x-button href="/jobs/{{ $job->id }}/edit" class='bg-blue-500 hover:bg-blue-600 text-white'>Edit
+                Job</x-button>
+        </p>
+    @endcan()
 </x-layout>
