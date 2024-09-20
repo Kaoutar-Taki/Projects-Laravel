5fe000292d73dcc94a64291c16d7ec272f6298b2
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-09-20 12:16:14 +0100

    migration

--- laracasts/database/migrations/2024_09_09_143738_create_employers_table.php
+++ laracasts/database/migrations/2024_09_09_143738_create_employers_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(User::class);
             $table->string('name');
+            $table->string('logo');
             $table->timestamps();
         });
     }
--- laracasts/database/migrations/2024_09_19_160713_create_job_tag_table.php
+++ laracasts/database/migrations/2024_09_19_160713_create_job_tag_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Job;
+use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,11 +13,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listings', function (Blueprint $table) {
+        Schema::create('job_tag', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('employer_id');
-            $table->string('title');
-            $table->string('salary');
+            $table->foreignIdFor(Job::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Tag::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listings');
+        Schema::dropIfExists('job_tag');
     }
 };
